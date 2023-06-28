6e3e5763c1df08fd81af8ad3f8cbba1bdf524eda
Autumn Fykes <autumn_fykes@brown.edu.com>
2023-06-27 21:05:26 -0400

    lol

---
+++ task1.py
@@ -0,0 +1,6 @@
+hours = input("How many hours would you like to calculate?")
+x=int(hours+60)
+minutes=str(x)
+print(minutes)
+
+#DNF
---
+++ task3.py
@@ -0,0 +1,4 @@
+string=input("Enter word: ")
+letters=len(string)
+x=str(letters)
+print("Number of letters: ", x)
